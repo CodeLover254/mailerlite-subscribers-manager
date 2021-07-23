ec1288ececd85d64301f07f14d3691194aec3645
CodeLover254 <astroguypasa@gmail.com>
2021-07-23 05:50:13 +0300

    Created SubscriberController responsible for handling management of subscriber information.

---
+++ app/Http/Controllers/SubscriberController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubscriberController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function showAddSubscriberForm()
+    {
+
+    }
+
+    public function storeNewSubscriber(Request $request)
+    {
+
+    }
+
+    public function showSubscriberEditForm()
+    {
+
+    }
+
+    public function updateSubscriber(Request $request)
+    {
+
+    }
+
+    public function deleteSubscriber(Request $request)
+    {
+
+    }
+}
