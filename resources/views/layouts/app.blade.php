71a05d7eed0bbd70012ce5d0c4e0e596756ce505
CodeLover254 <astroguypasa@gmail.com>
2021-07-26 18:54:29 +0300

    Created a base layout from with other layouts extended

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mailerlite Subscribers</title>
+    @include('layouts.styles')
+    @section('other-styles')
+        @show
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-success">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1 text-white">Mailerlite Subscribers Manager</span>
+    </div>
+</nav>
+
+@section('content')
+    @show
+
+
+@include('layouts.scripts')
+@section('other-scripts')
+    @show
+</body>
+</html>
