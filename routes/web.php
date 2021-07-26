b5f921fd07e73d113eb2d352d94b0543c39312f8
CodeLover254 <astroguypasa@gmail.com>
2021-07-26 19:27:30 +0300

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ApiUserController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\SubscriberController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ApiUserController::class,'index'])->middleware('serviceGuest')->name('index');
+Route::post('/', [ApiUserController::class,'validateKey'])->name('validate-api-key');
+Route::group(['middleware' => 'hasAPIKey'],function(){
+    Route::get('/add-group',[GroupController::class,'showAddGroupForm'])->name('show-add-group');
+    Route::post('/store-group',[GroupController::class,'storeNewGroup'])->name('store-group');
+});
+Route::group(['middleware' => ['hasAPIKey','hasGroup']],function(){
+    Route::get('/subscribers',[SubscriberController::class,'index'])->name('subscribers');
+    Route::get('/get-subscribers-list',[SubscriberController::class,'getSubscribers'])->name('paginated-subscribers');
+    Route::get('/add-subscriber',[SubscriberController::class,'showAddSubscriberForm'])->name('show-add-subscriber');
+    Route::post('/store-subscriber',[SubscriberController::class,'storeNewSubscriber'])->name('store-subscriber');
+    Route::get('/edit-subscriber/{email}',[SubscriberController::class,'showSubscriberEditForm'])->name('show-edit-subscriber');
+    Route::post('/update-subscriber/{email}',[SubscriberController::class,'updateSubscriber'])->name('update-subscriber');
+    Route::post('/delete-subscriber',[SubscriberController::class,'deleteSubscriber'])->name('delete-subscriber');
 });
